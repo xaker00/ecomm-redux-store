032360ee5eff64a70432d458de266f5275113e74
Ilya Spivakov <xaker00@gmail.com>
2021-11-20 21:30:46 -0800

    refactored to redux

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -15,7 +15,9 @@ import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,10 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+import React from "react";
+import { Provider } from "react-redux";
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+// Import Redux store from the file we created in /utils
+import store from "./store";
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
-
-  return <Provider value={[state, dispatch]} {...props} />;
-};
-
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
-
-export { StoreProvider, useStoreContext };
+export function StoreProvider(props) {
+  // Return the provider component provided by react-redux
+  return <Provider store={store} {...props} />;
+}
